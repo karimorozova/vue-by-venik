826ca89f3e48c8fca79777ca533e0336e67463b4
karine-morozova <morozova_karine@ukr.net>
2022-04-07 15:48:12 +0300

    Added icons and images

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -3,6 +3,8 @@
   <MyTitle></MyTitle>
   <h2>{{ title  }}</h2>
   <MyButton @click="incrementClicks" :outlined="true">Click me</MyButton>
+  <DirectivesTest />
+  <StarRating />
   <!-- <MyButton @click="incrementClicks" :outlined="true">Click me</MyButton> -->
   <!-- <MyButton @click="incrementClicks" outlined>Click me</MyButton> -->
  </div>
@@ -11,12 +13,16 @@
 <script>
 import MyTitle from '@/components/MyTitle.vue'
 import MyButton from '@/components/MyButton.vue'
+import DirectivesTest from '@/components/DirectivesTest.vue'
+import StarRating from '@/components/StarRating.vue'
 
 export default {
   name: 'App',
   components: {
    MyTitle,
-   MyButton
+   MyButton,
+   DirectivesTest,
+   StarRating
   },
   data() {
     return {
---
+++ my-app/src/components/DirectivesTest.vue
@@ -0,0 +1,41 @@
+<template>
+    <div v-if="isVisible">
+        <h1 v-show="false">I'm visible</h1>
+        <ul>
+            <template v-for="fruit in fruits">
+                <li v-if="fruit === 'banana'" :key="fruit">{{fruit }}</li>
+            </template>
+            
+        </ul>
+        <ul>
+            <li v-for="car in cars" :key="car.color">{{car.color }}</li>
+        </ul>
+         <ul>
+            <li v-for="(car, key) in cars" :key="key">{{key }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'DirectivesTest',
+        data() {
+            return {
+                isVisible: true,
+                fruits: ['apple', 'banana', 'kiwi'],
+                cars: {
+                    bmw: {
+                        color: 'red'
+                    },
+                    audi: {
+                        color: 'blue'
+                    }
+                }
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
