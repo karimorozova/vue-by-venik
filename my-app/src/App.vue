703d7e822a1945e6026860e6d61e3848dc7d1698
karine-morozova <morozova_karine@ukr.net>
2022-04-07 21:33:45 +0300

    Added ApartmentsItem component

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -1,10 +1,15 @@
 <template>
  <div :id="$style.app">
-  
-  <h2>{{ title  }}</h2>
+
+  <ApartmentsItem
+  :description="apartment.descr"
+  :rating="apartment.rating" 
+  :price="apartment.price"
+  :imgSrc="apartment.imgSrc"
+  />
   <!-- <MyButton @click="incrementClicks" :outlined="true">Click me</MyButton> -->
   
-  <StarRating :rating="3.4"/>
+  <!-- <StarRating :rating="3.4"/> -->
   <!-- <MyButton @click="incrementClicks" :outlined="true">Click me</MyButton> -->
   <!-- <MyButton @click="incrementClicks" outlined>Click me</MyButton> -->
  </div>
@@ -13,34 +18,38 @@
 <script>
 // import MyButton from '@/components/MyButton.vue'
 
-import StarRating from '@/components/StarRating.vue'
+// import StarRating from '@/components/StarRating.vue'
+import ApartmentsItem from '@/components/apartment/ApartmentsItem.vue'
 
 export default {
   name: 'App',
   components: {
-   StarRating
+  //  StarRating,
+   ApartmentsItem
   },
   data() {
     return {
-      amountOfClicks: 0
+      apartment: {
+        id: 1,
+        descr: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius, aspernatur?Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius, aspernatur?',
+        price: 3000,
+        rating: 3.5,
+        imgSrc: 'https://www.timeoutdubai.com/cloud/timeoutdubai/2021/09/11/hfpqyV7B-IMG-Dubai-UAE.jpg'
+
       }
+    }
   },
   computed: {
-    title() {
-      return `Amount of clicks: ${this.amountOfClicks}`
-    }
+
   },
   methods: {
-    incrementClicks() {
-      this.amountOfClicks += 1
-    }
   }
 }
 </script>
 
 <style module>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Montserrat, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- my-app/src/main.js
+++ my-app/src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import '@/assets/scss/index.scss'
 // import Vue from 'vue'
 
 // Vue.config.productionTip = false
